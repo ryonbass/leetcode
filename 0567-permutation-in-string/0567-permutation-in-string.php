ce709f8d7ffb42eaf9a9dbb4d80a1620609152e7
ryonbass <99014606+ryonbass@users.noreply.github.com>
2023-02-04 20:38:07 +0900

    Time: 1247 ms (50.00%), Space: 19.9 MB (23.08%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.php
+++ 0567-permutation-in-string/0567-permutation-in-string.php
@@ -7,19 +7,15 @@ class Solution {
      */
     function checkInclusion($s1, $s2) {
         if(strlen($s) === 1){
-            if(sttrpos($s2,$s1)!==false){
-                return true;
-            }
+            return sttrpos($s2,$s1)!==false ?: false;
         }
         $len1 = strlen($s1);
         $len2 = strlen($s2);
-        $arr_s1 = str_split($s1);
-        $s1_freq = array_count_values($arr_s1);
+        $s1_freq = array_count_values(str_split($s1));
         
         for($i=0;$i<$len2-$len1+1;$i++){
             $sub_word = substr($s2,$i,$len1);
-            $sub_arr = str_split($sub_word);
-            $sub_freq = array_count_values($sub_arr);
+            $sub_freq = array_count_values(str_split($sub_word));
             if($s1_freq == $sub_freq){
                 return true;
             }
