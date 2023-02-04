12b7e293fb4b2dbb0f667981a76854be35cd0638
ryonbass <99014606+ryonbass@users.noreply.github.com>
2023-02-04 20:32:32 +0900

    Time: 1103 ms (50.00%), Space: 20.2 MB (15.38%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.php
@@ -0,0 +1,29 @@
+class Solution {
+
+    /**
+     * @param String $s1
+     * @param String $s2
+     * @return Boolean
+     */
+    function checkInclusion($s1, $s2) {
+        if(strlen($s) === 1){
+            if(sttrpos($s2,$s1)!==false){
+                return true;
+            }
+        }
+        $len1 = strlen($s1);
+        $len2 = strlen($s2);
+        $arr_s1 = str_split($s1);
+        $s1_freq = array_count_values($arr_s1);
+        
+        for($i=0;$i<$len2-$len1+1;$i++){
+            $sub_word = substr($s2,$i,$len1);
+            $sub_arr = str_split($sub_word);
+            $sub_freq = array_count_values($sub_arr);
+            if($s1_freq == $sub_freq){
+                return true;
+            }
+        }
+        return false;
+    }
+}
