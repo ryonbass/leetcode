e276b927796a6ed1b967480e742d16ccc1b26f26
ryonbass <99014606+ryonbass@users.noreply.github.com>
2023-03-21 20:03:09 +0900

    Time: 359 ms (100.00%), Space: 32 MB (100.00%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.php
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.php
@@ -6,27 +6,16 @@ class Solution {
      */
     function zeroFilledSubarray($nums) {
         $cnt = count($nums);
-        $subArrlen = 0;
-        $sum = 0;
+        $subArrCnt = 0;
+        $ans = 0;
         for($i=0;$i<$cnt;$i++){
             if($nums[$i]===0){
-                $subArrlen++;
+                $subArrCnt++;
+                $ans += $subArrCnt;
                 continue;
             }
-            if($subArrlen!==0){
-                $subStack[$i] = $subArrlen;
-                $subArrlen = 0;
-            }       
+            $subArrCnt = 0;    
         }
-        if($subArrlen!==0){
-            $subStack[$cnt-1] = $subArrlen;
-        }  
-        foreach($subStack as $v){
-            while($v>0){
-                $sum += $v;
-                $v--;
-            }
-        }
-        return $sum;
+        return $ans;
     }
 }
