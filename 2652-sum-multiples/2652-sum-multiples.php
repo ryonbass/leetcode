699cd9e13cb144adab0268613635010dd5680ebf
ryonbass <99014606+ryonbass@users.noreply.github.com>
2023-05-25 21:42:27 +0900

    Time: 27 ms (81.82%), Space: 19.1 MB (75.76%) - LeetHub

---
+++ 2652-sum-multiples/2652-sum-multiples.php
@@ -0,0 +1,16 @@
+class Solution {
+
+    /**
+     * @param Integer $n
+     * @return Integer
+     */
+    function sumOfMultiples($n) {
+        $sum = 0;
+        for($i=1;$i<=$n;$i++){
+            if($i%3==0 || $i%5==0 || $i%7==0){
+                $sum += $i;
+            }
+        }
+        return $sum;
+    }
+}
